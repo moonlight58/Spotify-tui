bd5e7199629e290fdc1d117a8f707dc9aa67134c
moonlight58 <gaelrothlin@gmail.com>
2025-06-01 22:02:17 +0200

    Enhance event handling for search and library selection modes

--- src/event.c
+++ src/event.c
@@ -30,44 +30,79 @@ void handle_events(WINDOW **search_bar, WINDOW **help_bar,
         TuiWindow *current = get_window(focused_window);
         int nx = current->grid_x, ny = current->grid_y;
 
+        // --- Mode sélection : Search bar ---
+        if (selector_mode == 1) {
+            if (ch == 27) { // ESC
+                selector_mode = 0;
+                mvwprintw(get_window(0)->window, 1, 2, "%*s", 50, " ");
+                wrefresh(get_window(0)->window);
+            } else if (ch == '\n' || ch == KEY_ENTER) {
+                do_search(search_input);
+                selector_mode = 0;
+            } else if (ch == KEY_BACKSPACE || ch == 127) {
+                int len = strlen(search_input);
+                if (len > 0) search_input[len-1] = '\0';
+            } else if (isprint(ch) && strlen(search_input) < sizeof(search_input)-1) {
+                int len = strlen(search_input);
+                search_input[len] = ch;
+                search_input[len+1] = '\0';
+            }
+            mvwprintw(get_window(0)->window, 1, 2, "Search: %-48s", search_input);
+            wrefresh(get_window(0)->window);
+            continue;
+        }
+
+        // --- Mode sélection : Library ---
+        if (selector_mode == 2) {
+            if (ch == 27) { // ESC
+                selector_mode = 0;
+            } else if (ch == KEY_UP) {
+                if (selector_index > 0) selector_index--;
+            } else if (ch == KEY_DOWN) {
+                if (selector_index < library_count-1) selector_index++;
+            } else if (ch == '\n' || ch == KEY_ENTER) {
+                do_library_action(selector_index);
+                selector_mode = 0;
+            }
+            render_library_with_selector(get_window(2)->window, library_items, library_count, selector_index);
+            render_windows_with_focus(focused_window);
+            continue;
+        }
+
+        // --- Mode sélection : Playlist (à compléter si besoin) ---
+        if (selector_mode == 3) {
+            // Ajoute ici la gestion de la sélection de playlist si besoin
+            continue;
+        }
+
+        // --- Navigation entre fenêtres UNIQUEMENT si pas en mode sélection ---
         switch (ch)
         {
         case KEY_RESIZE:
-            // Get new terminal size
             getmaxyx(stdscr, height, width);
-            // Recalculate layout
             calculate_layout(height, width, layouts);
-
-            // Delete old windows
             delwin(*search_bar);
             delwin(*help_bar);
             delwin(*library_win);
             delwin(*playlist_win);
             delwin(*main_win);
             delwin(*progress_bar);
-
-            // Create new windows
             *search_bar = create_window_with_layout(layouts[0], 1, "Search");
             *help_bar = create_window_with_layout(layouts[5], 2, "Help");
             *library_win = create_window_with_layout(layouts[1], 3, "Library");
             *playlist_win = create_window_with_layout(layouts[2], 4, "Playlists");
             *main_win = create_window_with_layout(layouts[3], 5, "Welcome !");
             *progress_bar = create_window_with_layout(layouts[4], 1, "Progress Bar");
-
             mvwprintw(*help_bar, 1, 1, "Type ?");
-
-            // Met à jour les pointeurs dans windows[]
             init_windows(*search_bar, *help_bar, *library_win, *playlist_win, *main_win, *progress_bar);
             break;
         case '?':
-            // Open help window with the entire list of commands
             wrefresh(*help_bar);
             break;
         case 's':
             mvwprintw(*search_bar, 1, 1, "Search: ");
             wrefresh(*search_bar);
             break;
-
         case KEY_UP:
             ny--;
             break;
@@ -85,10 +120,7 @@ void handle_events(WINDOW **search_bar, WINDOW **help_bar,
         }
 
         int next = find_window_by_grid(nx, ny);
-        // Skip non-focusable windows
         while (next != -1 && !get_window(next)->is_focusable) {
-            // Continue in the same direction
-            // Example for KEY_UP:
             if (ch == KEY_UP) ny--;
             else if (ch == KEY_DOWN) ny++;
             else if (ch == KEY_LEFT) nx--;
@@ -98,64 +130,27 @@ void handle_events(WINDOW **search_bar, WINDOW **help_bar,
         if (next != -1)
             focused_window = next;
 
-        if (!selector_mode && (ch == '\n' || ch == KEY_ENTER)) {
+        // --- Entrée dans un mode sélection ---
+        if (ch == '\n' || ch == KEY_ENTER) {
             switch (focused_window) {
                 case 0: // Search bar
                     selector_mode = 1;
                     search_input[0] = '\0';
-                    // Optionally, show a prompt
                     mvwprintw(get_window(0)->window, 1, 2, "Search: ");
                     wrefresh(get_window(0)->window);
                     break;
                 case 2: // Library
                     selector_mode = 2;
                     selector_index = 0;
-                    // Highlight first item
+                    render_library_with_selector(get_window(2)->window, library_items, library_count, selector_index);
                     break;
                 case 3: // Playlist
                     selector_mode = 3;
                     selector_index = 0;
-                    // Highlight first playlist
+                    // Ajoute ici le rendu de la playlist si besoin
                     break;
             }
-        }
-
-        if (selector_mode == 1) {
-            if (ch == 27) { // ESC
-                selector_mode = 0;
-                // Clear prompt
-                mvwprintw(get_window(0)->window, 1, 2, "%*s", 50, " ");
-                wrefresh(get_window(0)->window);
-            } else if (ch == '\n' || ch == KEY_ENTER) {
-                // Call your search function
-                do_search(search_input);
-                selector_mode = 0;
-            } else if (ch == KEY_BACKSPACE || ch == 127) {
-                int len = strlen(search_input);
-                if (len > 0) search_input[len-1] = '\0';
-            } else if (isprint(ch) && strlen(search_input) < sizeof(search_input)-1) {
-                int len = strlen(search_input);
-                search_input[len] = ch;
-                search_input[len+1] = '\0';
-            }
-            // Update display
-            mvwprintw(get_window(0)->window, 1, 2, "Search: %-48s", search_input);
-            wrefresh(get_window(0)->window);
-            continue; 
-        }
-
-        if (selector_mode == 2) {
-            if (ch == 27) { // ESC
-                selector_mode = 0;
-            } else if (ch == KEY_UP) {
-                if (selector_index > 0) selector_index--;
-            } else if (ch == KEY_DOWN) {
-                if (selector_index < library_count-1) selector_index++;
-            } else if (ch == '\n' || ch == KEY_ENTER) {
-                do_library_action(selector_index);
-                selector_mode = 0;
-            }
-            render_library_with_selector(get_window(2)->window, library_items, library_count, selector_index);
+            render_windows_with_focus(focused_window);
             continue;
         }
 
@@ -185,6 +180,5 @@ void do_search(const char *query) {
 
 void do_library_action(int index) {
     // TODO: Handle library item selection
-    mvprintw(1, 0, "Selected library item: %d", index);
-    refresh();
+    // mvprintw(1, 0, "Selected library item: %d", index);
 }
